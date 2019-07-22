b74132d26ed8bf244f24e9841828d956ac772fe8
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-22 23:58:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,6 +54,17 @@ attr_accessor :id, :name, :breed
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
+  def self.find_or_create_by(name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? and breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = self.create(name:, breed:)
+    end
+    dog
+  end
+
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
