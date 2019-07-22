1d088833d44864eff1add41d70c5b0fa895cbcc0
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-22 23:47:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,7 +35,7 @@ attr_accessor :id, :name, :breed
   end
 
   def self.create(name:, breed:)
-    dog = Dog.new(name, breed)
+    dog = Dog.new(name: name, breed: breed)
     dog.save
     dog
   end
