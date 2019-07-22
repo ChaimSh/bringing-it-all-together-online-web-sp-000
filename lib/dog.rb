04f1dbf04aa6a3000bb7d1cae2b66fb76c2ebab3
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-22 23:39:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,10 +44,10 @@ attr_accessor :id, :name, :breed
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    breed = row[2]
-    self.new(id:, name:, breed:)
+    id: row[0]
+    name: row[1]
+    breed: row[2]
+    self.new(id, name, breed)
   end
 
   def self.find_by_name(name)
